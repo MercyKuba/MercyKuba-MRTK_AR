5947d3f3226b98e19f358a9cda81b800eca42a56
MercyKuba <akuwork0801@gmail.com>
2021-04-02 01:25:13 -0500

    First commit

---
+++ Assets/MyProject/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    private GameObject[] placedPrefab;
+
+
+    public GameObject[] PlacedPrefab
+    {
+        get { return placedPrefab; }
+        set { placedPrefab = value; }
+    }
+
+    public GameObject spawnedObject { get; private set; }
+
+    public static event Action onPlacedObject;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+    int i = 0;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    i++;
+                    Pose hitPose = s_Hits[0].pose;
+
+                    spawnedObject = Instantiate(placedPrefab[i % placedPrefab.Length], hitPose.position, hitPose.rotation);
+
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+    }
+}
